f22daa7cee23a1809d45dc2f8f59d30a7e78c48b
OtetcSergy <zblot133@gmail.com>
2020-03-22 19:59:33 +0300

    the-seventh-lesson__"animation in JQuery"

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 $(document).ready(function(){
 
-
-  $('#btn-1').on('dblclick' , function(){
-    $('h1').toggleClass('pink');
-  });   
-
-  $('#text-area').on('mouseenter' , function(){
-    $(this).toggleClass('change-color');
-  });
-
-  $('#text-area').on('mouseleave' , function(){
-    $(this).toggleClass('change-color');
-  });
-
-  $('#input-1').on('focus' , function(){
-    $(this).toggleClass('active');
-  });
-
-  $('#input-1').on('blur' , function(){
-    $(this).toggleClass('active');
-  });
-
-  $('#input-1').on('change' , function(){
-    $('#text-holder').text( $(this).val());
+  $('#customWidth').on('click' , function(){
+    $('#test').animate({'width': '200px', }, 4000);
   })
 
-  $('#input-1').on('keyup' , function(){
-    $('#text-holder').text( $(this).val());
+  $('#customOpacity').on('click', function(){
+    $('#test').animate({'opacity': '0.5'}, 4000);
   })
 
-  $(#input-1).on('keyup' , function(e) {
-    if ( e.which == 27) {
-      $('#text-holder').text('');
-    }
+  $('#hide').on('click', function(){
+    $('#test').hide(100);
   })
 
-  
+  $('#show').on('click', function(){
+    $('#test').show(1000);
+  })
+
+  $('#hideAndShow').on('click', function(){
+    $('#test').hide(1000).delay(1000).show(1000);
+  })
+
+  $('#fadeOut').on('click', function(){
+    $('#test').fadeOut(1000);
+  })
+
+  $('#fadeIn').on('click', function(){
+    $('#test').fadeIn(1000);
+  })
+
+  $('#fadeToggle').on('click', function(){
+    $('#test').fadeToggle(1000);
+  })
+
+  $('#slideUp').on('click', function(){
+    $('#test').slideUp(1000);
+  })
+
+  $('#slideDown').on('click', function(){
+    $('#test').slideDown(1000);
+  })
+
+  $('#slideToggle').on('click', function(){
+    $('#test').slideToggle(1000);
+  })
 });
