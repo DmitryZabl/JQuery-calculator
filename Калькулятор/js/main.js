afc437838ee668e6f2e05a6aac7b9865810dd9e1
OtetcSergy <zblot133@gmail.com>
2020-03-24 21:18:23 +0300

    the-10th-lesson__"page layout"

--- Калькулятор/js/main.js
+++ Калькулятор/js/main.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
 
-  $('#toggles div').on('click', function(){
-    let imgPath = $(this).attr('data-img-path');
-    let $img = $('#img-holder img');
+  $('#test-box').html('<p style= "margin-left: 40px;" >[eq</p>');
+  $('#test-box').before('<p style= "margin-left: 40px;" >[eq</p>');
 
-    $img.fadeOut(1000 , function(){
-      $img.attr('src', imgPath).fadeIn(400);
-    })
-  });
+  /* $newContent = $(
+    '<h4>Заголовок</h4>'  +
+    '<p>Абзац с текстом</p>' +
+    '<ul>' +
+      '<li>Пункт 1</li>' +
+      '<li>Пункт 2</li>' +
+      '<li>Пункт 3</li>' +
+    '</ul>'
+  );
+
+  $newContent.hide();
+  $('#test-box').html($newContent);
+  $('#test-box').on('click', function () {
+    $newContent.fadeIn(1000);
+  }) */
 
 
-});
+  $('#test-box').on('click', function() {
+    let $cloneElement = $(this).clone();
+    $cloneElement.toString  = function  ()  {
+      return 'это клон';
+    };
+    $cloneElement.empty().html('[e[e[e[e')
+    $(this).after($cloneElement);
+   
+  })
+  //$('#text-block2').unwrap().unwrap();
+
+  
+
+})
