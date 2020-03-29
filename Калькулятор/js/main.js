6cf914c38f3ee28a8fd39cd2bca620c607ac0e92
OtetcSergy <zblot133@gmail.com>
2020-03-29 23:14:28 +0300

    calc is done

--- Калькулятор/js/main.js
+++ Калькулятор/js/main.js
@@ -1,36 +1,69 @@
-$(document).ready(function(){
-
-  $('#test-box').html('<p style= "margin-left: 40px;" >[eq</p>');
-  $('#test-box').before('<p style= "margin-left: 40px;" >[eq</p>');
-
-  /* $newContent = $(
-    '<h4>Заголовок</h4>'  +
-    '<p>Абзац с текстом</p>' +
-    '<ul>' +
-      '<li>Пункт 1</li>' +
-      '<li>Пункт 2</li>' +
-      '<li>Пункт 3</li>' +
-    '</ul>'
-  );
-
-  $newContent.hide();
-  $('#test-box').html($newContent);
-  $('#test-box').on('click', function () {
-    $newContent.fadeIn(1000);
-  }) */
 
 
-/*   $('#test-box').on('click', function() {
-    let $cloneElement = $(this).clone();
-    $cloneElement.toString  = function  ()  {
-      return 'это клон';
-    };
-    $cloneElement.empty().html('[e[e[e[e')
-    $(this).after($cloneElement);
-   
-  }) */
-  //$('#text-block2').unwrap().unwrap();
 
-  
+$(document).ready(function () {
 
+  let rurUsdRate = 0;
+
+  $.ajax({
+    url: "https://www.cbr-xml-daily.ru/daily_json.js",
+    cashe: false,
+    success: function (data) {
+      data = JSON.parse(data);
+      rurUsdRate = data.Valute.USD.Value;
+      showPrice()
+    }
+  })
+
+  showPrice();
+  $('#show-info__dollars').text("Обновление курса валют")
+  showInfoContainer();
+
+  $('#formSettings input').on('change', function () {
+    showPrice();
+    showInfoContainer();
+  });
+
+  $('#choise-car div').on('click', function () {
+    let imgPath = $(this).attr('data-img-path');
+    carName = $(this).text();
+    $('#choise-car div').removeClass('choise-car__item--choised');
+    $(this).addClass('choise-car__item--choised');
+    $('#imgHolder').attr('src', imgPath);
+    $('#show-info__title').text(carName);
+    showPrice()
+  })
+
+
+
+
+  function calcPrice() {
+    let enginePrice = +$('input[name = engine]:checked', '#formSettings').val(),
+      transmissionPrice = +$('input[name = transmission]:checked', '#formSettings').val(),
+      equipmentPrice = +$('input[name = equipment]:checked', '#formSettings').val(),
+      modelPrice = +$('#choise-car div.choise-car__item--choised').attr('data-value');
+    return enginePrice + transmissionPrice + equipmentPrice + modelPrice;
+
+  }
+
+  function showInfoContainer() {
+    let checkedEngine = $('input[name = engine]:checked + label', '#formSettings').text(),
+      checkedTransmission = $('input[name = transmission]:checked + label', '#formSettings').text(),
+      checkedEquipment = $('input[name = equipment]:checked + label', '#formSettings').text();
+    $('#show-info__engine').text(checkedEngine);
+    $('#show-info__transmission').text(checkedTransmission);
+    $('#show-info__equipment').text(checkedEquipment);
+  }
+
+  function showPrice() {
+    let price = calcPrice(),
+      priceRu = price.toLocaleString('ru-RU') + ' рублей',
+      priceEn = '$ ' + convertPrice(price);
+    $('#show-info__rubls').text(priceRu);
+    $('#show-info__dollars').text(priceEn);
+  }
+
+  function convertPrice(price) {
+    return Math.round(price / rurUsdRate).toLocaleString();
+  }
 })
